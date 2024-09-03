6b2af0bbca799c79fd750d9affb9df210c4f61d6
j9mmy <127404933+j9mmy@users.noreply.github.com>
2024-09-04 01:23:28 +0200

    feat: switch/add conversations

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,6 @@ import { Message } from "@/components/message";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
-    Avatar,
-    AvatarFallback,
-} from "@/components/ui/avatar"
-import {
     DropdownMenu,
     DropdownMenuContent,
     DropdownMenuGroup,
@@ -16,6 +12,7 @@ import {
 } from "@/components/ui/dropdown-menu"
 import {
     Sheet,
+    SheetClose,
     SheetContent,
     SheetDescription,
     SheetHeader,
@@ -27,7 +24,7 @@ import {
     PopoverContent,
     PopoverTrigger,
 } from "@/components/ui/popover"
-import { Send, Menu, User, Settings, LogOut, ChevronDown, Plus, Key } from "lucide-react";
+import { Send, Menu, ChevronDown, Plus, Key, Settings, Eraser } from "lucide-react";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { useState } from "react";
 import { invoke } from "@tauri-apps/api";
@@ -44,76 +41,154 @@ interface HfResponse {
     generated_text: string;
 }
 
+type ModelProps = {
+    id: number,
+    name: string
+  }
+
+type ConversationProps = {
+    id: number;
+    name: string;
+    messages: MessageProps[];
+}
+
 function App() {
-    const [messages, setMessages] = useState<MessageProps[]>([]);
-    const [models, setModels] = useState([
-        "google/gemma-1.1-7b-it",
-        "mistralai/Mixtral-8x7B-Instruct-v0.1",
-        "meta-llama/Meta-Llama-3-70B-Instruct",
-        "mistralai/Mistral-Nemo-Instruct-2407",
-        "mistralai/Mistral-7B-Instruct-v0.3",
-        "openai-community/gpt2"
+    const [models, setModels] = useState<ModelProps[]>([
+        {
+        id: 0,
+        name: "mistralai/Mistral-7B-Instruct-v0.3",
+        },
+        {
+        id: 1,
+        name: "google/gemma-1.1-7b-it",
+        },
+        {
+        id: 2,
+        name: "openai-community/gpt2",
+        },
     ])
+    const [conversations, setConversations] = useState<ConversationProps[]>([
+        {
+            id: 0,
+            name: "Greetings",
+            messages: [
+                {
+                    id: 0,
+                    conversationId: 0,
+                    message: "Hi, how are you?",
+                    isAi: false,
+                },
+                {
+                    id: 1,
+                    conversationId: 0,
+                    message: "I'm good, how about you?",
+                    isAi: true,
+                }
+            ]
+        },
+        {
+            id: 1,
+            name: "Tomato discussion",
+            messages: [
+                {
+                    id: 3,
+                    conversationId: 1,
+                    message: "Is tomato a fruit or a vegetable?",
+                    isAi: false,
+                },
+                {
+                    id: 4,
+                    conversationId: 1,
+                    message: "A tomato is a fruit. Knowledge is knowing that a tomato is a fruit. Wisdom is not putting it in a fruit salad.",
+                    isAi: true,
+                }
+            ]
+        }
+    ])
+    const [conversation, setConversation] = useState<ConversationProps>(conversations[0])
     const [currentModel, setCurrentModel] = useState(models[0]);
-    const [inputField, setInputField] = useState("");
+    const [userInput, setUserInput] = useState("");
     const [responseLoading, setResponseLoading] = useState(false);
     const [key, setKey] = useState("");
 
     const sendMessage = async (message: string, isAi: boolean) => {
         setResponseLoading(true);
-        setInputField("");
+        setUserInput("");
+        
+        // Add a new message to the conversation and let the user know that the AI is thinking
+        const newMessage =  [
+            {
+                id: conversation.messages.length,
+                conversationId: conversation.id,
+                message: message,
+                isAi: isAi
+            },
+            {
+                id: conversation.messages.length + 1,
+                conversationId: conversation.id,
+                message: "Thinking...",
+                isAi: true
+            }
+        ]
+        let updatedMessages = [...conversation.messages, ...newMessage];
+        setConversation({ ...conversation, messages: updatedMessages });
     
-        // Create a new array with the user's message
-        const newMessages = [...messages, {
-            id: messages.length,
-            conversationId: 0,
-            message: message,
-            isAi: isAi
-        }];
-        setMessages(newMessages);
-    
-        // Add a "Thinking" message to the state
-        const thinkingMessage = {
-            id: newMessages.length,
-            conversationId: 0,
-            message: "Thinking...",
-            isAi: true
-        };
-        setMessages([...newMessages, thinkingMessage]);
-    
-        // Define the system prompt
-        const systemPrompt = "System: Always assist with care, respect, and truth. Respond with utmost utility yet securely. Avoid harmful, unethical, prejudiced, or negative content. Ensure replies promote fairness and positivity.";
-    
-        // Concatenate all previous messages into a single string with labels
-        const conversationHistory = systemPrompt + '\n' + newMessages.map(msg => `${msg.isAi ? 'AI' : 'User'}: ${msg.message}`).join('\n') + '\nAI:';
+        // System prompt for the AI to follow
+        const systemPrompt = `System: Always assist with care, respect, and truth. Respond with utmost utility yet securely. Avoid harmful, unethical, prejudiced, or negative content. Ensure replies promote fairness and positivity. Do not include "User: " in your responses. Only respond as the AI. \n\n`;
+        
+        // Concatenate all previous messages into a single prompt and label each message with their respective sender for better context
+        const conversationHistory = systemPrompt + updatedMessages.map((message) => {
+            return (message.isAi ? "AI: " : "User: ") + message.message;
+        }).join("\n");
         console.log(conversationHistory);
     
-        await invoke<HfResponse>('hf_request', { message: conversationHistory, model: currentModel, key: key })
+        // Send the conversation history to the AI model and get a response
+        await invoke<HfResponse>('hf_request', { message: conversationHistory, model: currentModel.name, key: key })
             .then((response) => {
-                console.log(response);
-                const trimmedGeneratedText = response.generated_text.trim();
-                // Replace the "Thinking" message with the actual AI-generated message
-                const updatedMessages = [...newMessages, {
-                    id: newMessages.length,
-                    conversationId: 0,
-                    message: trimmedGeneratedText,
-                    isAi: true,
-                }];
-                setMessages(updatedMessages);
+                // Clean up the response and avoid the AI speaking on behalf of the user
+                let trimmedGeneratedText = response.generated_text.trim();
+                const indexOfUser = trimmedGeneratedText.indexOf("User: ");
+                if (indexOfUser !== -1) {
+                    trimmedGeneratedText = trimmedGeneratedText.substring(0, indexOfUser);
+                }
+
+                // Replace the "Thinking..." message with the AI's response
+                updatedMessages[updatedMessages.length - 1].message = trimmedGeneratedText;
+
+                updateConversation(updatedMessages);
+                
             })
             .catch((error) => {
                 console.error(error);
-                const updatedMessages = [...newMessages, {
-                    id: newMessages.length,
-                    conversationId: 0,
-                    message: "An error occurred while processing your request.",
-                    isAi: true,
-                }];
-                setMessages(updatedMessages);
+                updatedMessages[updatedMessages.length - 1].message = "An error occurred while processing your request.";
+
+                updateConversation(updatedMessages);
             });
     
         setResponseLoading(false);
     }
+
+    const updateConversation = (updatedMessages: MessageProps[]) => {
+        // Update the active conversation with the new messages
+        setConversation({ ...conversation, messages: updatedMessages });
+
+        // Update the active conversation in the list of conversations
+        setConversations(conversations.map((conv) => conv.id === conversation.id ? conversation : conv));
+    }
+
+    const addConversation = () => {
+        const conversationObject = 
+        {
+            id: conversations.length,
+            name: "New Conversation",
+            messages: []
+        }
+
+        const newConversations = [...conversations, conversationObject]
+        setConversations(newConversations)
+        setConversation(conversationObject)
+    }
+
 
     return (
         <div className="flex flex-col h-screen py-2">
@@ -123,55 +198,78 @@ function App() {
                         <SheetTrigger>
                             <Button variant={"ghost"} size={"icon"}><Menu size={16}/></Button>
                         </SheetTrigger>
-                        <SheetContent side={"left"} className="h-fit w-fit m-4 p-0 rounded-lg">
-                            <SheetHeader className="p-4 pb-2 pt-3">
-                                <SheetTitle className="text-xl font-extrabold">Babble</SheetTitle>
+                        <SheetContent side={"left"} className="flex flex-col h-fit m-4 p-0 rounded-lg">
+                            <SheetHeader className="p-4 pb-0 pt-3">
+                                <SheetTitle className="font-bold">Babble</SheetTitle>
                             </SheetHeader>
-                            <hr className="w-full h-0.5 bg-stone-100" />
-                            <SheetDescription className="p-4 py-2 text-stone-900 font-bold">Previous Babbles</SheetDescription>
                             <div className="w-full pb-2">
-                                <Button className="w-full justify-start rounded-none" variant="ghost">Trip to Italy</Button>
-                                <Button className="w-full justify-start rounded-none" variant="ghost">Digital Nomadism</Button>
-                                <Button className="w-full justify-start rounded-none" variant="ghost">Test Article</Button>
-                            </div>
-                            <hr className="w-full h-0.5 bg-stone-100" />
-                            <SheetDescription className="p-4 py-2 text-stone-900 font-bold">Miscellaneous</SheetDescription>
-                            <div className="w-full pb-2">
-                                <Button className="w-full justify-start rounded-none" variant="ghost">Help</Button>
-                                <Button className="w-full justify-start rounded-none" variant="ghost">Feedback</Button>
-                                <Button className="w-full justify-start rounded-none" variant="ghost">About</Button>
-                                <Button className="w-full justify-start rounded-none" variant="ghost">GitHub</Button>
+                                <SheetClose asChild>
+                                    <Button 
+                                            className="flex flex-row gap-2 justify-start w-full mb-2 rounded-none"
+                                            variant={"ghost"}
+                                            onClick={() => addConversation()}
+                                        >
+                                            <Plus size={16}/>
+                                            New conversation
+                                    </Button>
+                                </SheetClose>
+                                {conversations.map((conversation, index) => (
+                                    <SheetClose asChild>
+                                        <Button 
+                                            className="w-full justify-start rounded-none font-normal" 
+                                            variant="ghost"
+                                            key={index}
+                                            onClick={() => setConversation(conversation)}
+                                        >
+                                            {conversation.name}
+                                        </Button>
+                                    </SheetClose>
+                            ))}
                             </div>
                         </SheetContent>
                     </Sheet>
-                    <Button size={"icon"} variant="ghost"><Plus size={16}/></Button>
+                    <Button 
+                        size={"icon"} 
+                        variant={"ghost"}
+                        onClick={() => setConversation({
+                            id: conversation.id,
+                            name: conversation.name,
+                            messages: []
+                        })}
+                    >
+                        <Eraser size={16} />
+                    </Button>
                 </div>
                 <div className="flex flex-row items-center justify-center w-full">
-                    <p className="w-max text-sm">You are now babbling    to</p>
-                    <DropdownMenu>
-                        <DropdownMenuTrigger asChild>
-                            <Button variant="link" className="font-bold px-1 h-auto gap-1">
-                                {currentModel}
-                                <ChevronDown size={12} />
-                            </Button>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent className="w-auto">
-                            <DropdownMenuLabel>Switch model</DropdownMenuLabel>
-                            <DropdownMenuSeparator />
-                            <DropdownMenuGroup>
-                                {models.map((model, index) => (
-                                    <DropdownMenuItem key={index} onSelect={() => setCurrentModel(model)}>
-                                        <span>{model}</span>
-                                    </DropdownMenuItem>
-                                ))}
+                    <p className="w-max text-sm">You are now babbling to</p>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <Button variant="link" className="font-bold px-1 h-auto gap-1">
+                                    {currentModel.name}
+                                    <ChevronDown size={12} />
+                                </Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent className="w-auto">
+                                <DropdownMenuLabel>Switch model</DropdownMenuLabel>
                                 <DropdownMenuSeparator />
-                                <DropdownMenuItem>
-                                    <Plus className="mr-2 h-4 w-4" />
-                                    <span>Add new model</span>
-                                </DropdownMenuItem>
-                            </DropdownMenuGroup>
-                        </DropdownMenuContent>
-                    </DropdownMenu>
+                                <DropdownMenuGroup>
+                                    {models.map((model, index) => (
+                                        <DropdownMenuItem key={index} onSelect={() => setCurrentModel(model)}>
+                                            <span>{model.name}</span>
+                                        </DropdownMenuItem>
+                                    ))}
+                                    <DropdownMenuSeparator />
+                                        <DropdownMenuItem>
+                                            <Plus className="mr-2 h-4 w-4" />
+                                            <span>Add new model</span>
+                                        </DropdownMenuItem>
+                                    <DropdownMenuItem>
+                                        <Settings className="mr-2 h-4 w-4" />
+                                        <span>Manage models</span>
+                                    </DropdownMenuItem>
+                                </DropdownMenuGroup>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
                 </div>
                 <div className="flex flex-row w-full me-2 justify-end">
                     <Popover>
@@ -179,22 +277,22 @@ function App() {
                             <Button size={"icon"} variant="ghost"><Key size={16}/></Button>
                         </PopoverTrigger>
                         <PopoverContent side={"bottom"} className="w-48">
-                            <Input type="password" value={key} onChange={(e) => setKey(e.target.value)} className="bg-stone-100" placeholder="API Key" />
+                            <Input type="password" value={key} onChange={(e) => setKey(e.target.value)} className="bg-stone-100" placeholder="Access Token" />
                         </PopoverContent>
                     </Popover>
                 </div>
             </header>
             <ScrollArea className="h-full">
                 <div className="flex flex-col gap-2 h-full max-w-screen-md mx-auto px-8 mb-8">
-                    {messages.map((message, index) => (
+                    {conversation.messages.map((message, index) => (
                         <Message key={index} message={message.message} isAi={message.isAi} />
                     ))}
                 </div>
             </ScrollArea>
             <footer className="flex justify-center items-center w-full p-2 pt-2">
                 <div className="flex flex-row max-w-screen-md w-full gap-2">
-                    <Input value={inputField} onChange={(e) => setInputField(e.target.value)} className="bg-stone-100" placeholder={"Babble to " + currentModel} />
-                    <Button disabled={responseLoading} onClick={() => sendMessage(inputField, false)} className="p-3"><Send size={16}/></Button>
+                    <Input value={userInput} onChange={(e) => setUserInput(e.target.value)} className="bg-stone-100" placeholder={"Babble to " + currentModel.name} />
+                    <Button disabled={responseLoading} onClick={() => sendMessage(userInput, false)} className="p-3"><Send size={16}/></Button>
                 </div>
             </footer>
         </div>
