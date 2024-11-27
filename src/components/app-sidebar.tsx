babda148526b22ee43764a2d72ff942fc62e3fa1
j9mmy <jimmy.tukiran@outlook.com>
2024-11-27 03:02:57 +0100

    fix: remove unimplemented feature

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -46,7 +46,6 @@ export function AppSidebar() {
           </SidebarMenuItem>
           <ChangeModelMenuItem />
           <ChangeAccessTokenMenuItem />
-          <SettingsMenuItem />
         </SidebarMenu>
       </SidebarFooter>
 
